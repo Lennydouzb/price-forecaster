26037d2b34972d92e840eb45b1d4d42a38014622
Lennydouzb <lennydesbouis.pro@gmail.com>
2026-05-04 17:20:20 +0200

    parsing

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import panda as pd
+import pandas as pd
 
 
-data = pd.read_csv("2025.txt", sep="|", decimal = ",", low_memory = False)
-data_gir = data[data["Code departement"].astype(str) == "33"].copy
-data_gir = data_gir[data_gir["Type local"].astype(str).isin(["Appartement", "Maison"])]
+data = pd.read_csv("2025.csv", sep="|", decimal = ",", low_memory = False)
+data_gir = data[data["Code departement"].astype(str) == "33"].copy()
+data_gir = data_gir[data_gir["Type local"].isin(["Appartement", "Maison"])]
 data_gir = data_gir.dropna(subset=["Valeur fonciere", "Surface reelle bati", "Surface terrain"])
 col = ["Valeur fonciere", "Code postal", "Commune", "Type local", "Surface reelle bati", "Nombre pieces principales", "Surface terrain"]
 
-dataset = data_git[col]
-print (dataset.len())
-print (ddataset.len)
-
+dataset = data_gir[col]
+print (len(dataset))
+print (dataset.head())
